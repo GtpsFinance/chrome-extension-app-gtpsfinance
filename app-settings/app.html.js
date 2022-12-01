d6423f900693a36007911913b8a0a442876a2355
Gtps.Finance <welcome@gtps.finance>
2022-12-01 17:28:23 +0000

    Add files via upload

---
+++ app-settings/app.html.js
@@ -0,0 +1,95 @@
+import { html } from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+export function getTemplate() {
+    return html `<!--_html_template_start_--><style include="app-management-shared-style">
+  #content {
+    background-color: var(--cr-card-background-color);
+    border-radius: var(--cr-card-border-radius);
+    box-shadow: var(--cr-card-shadow);
+    color: var(--cr-primary-text-color);
+    /* Follows margin used in chrome://history */
+    margin-top: 24px;
+  }
+
+  /* Follows styles in settings_subpage for header and app title */
+  #headerLine {
+    min-height: 40px;
+    padding-bottom: 24px;
+    padding-top: 8px;
+  }
+
+  #title-icon {
+    height: 36px;
+    margin-inline-end: 12px;
+    margin-inline-start: 2px;
+    width: 36px;
+  }
+
+  #uninstall-button {
+    /* Push the button to the right in flex layout. */
+    margin-inline-start: auto;
+  }
+</style>
+<cr-toolbar page-name="App settings" show-search="[[showSearch_]]"
+    always-show-logo>
+</cr-toolbar>
+<div id="content" class="cr-centered-card-container">
+  <div class="cr-row first" id="headerLine">
+    <img id="title-icon" src="[[iconUrl_]]" aria-hidden="true">
+    <h1 class="cr-title-text">[[app_.title]]</h1>
+    <app-management-uninstall-button id="uninstall-button"
+        app="[[app_]]"
+        uninstall-label="Uninstall"
+        policy-label="This app has been installed by your administrator.">
+    </app-management-uninstall-button>
+  </div>
+  <div class="permission-list">
+    <app-management-run-on-os-login-item
+      class="permission-card-row separated-row"
+      login-mode-label="Start app when you sign in"
+      app="[[app_]]">
+    </app-management-run-on-os-login-item>
+    <app-management-window-mode-item
+      class="permission-card-row separated-row"
+      window-mode-label="Open as window"
+      app="[[app_]]">
+    </app-management-window-mode-item>
+    <app-management-permission-item
+        class="permission-card-row separated-row"
+        app="[[app_]]"
+        permission-label="Notifications"
+        permission-type="kNotifications">
+    </app-management-permission-item>
+    <div id="permissions-card" class="permission-card-row">
+      <div class="permission-section-header">
+        <div class="header-text">Permissions</div>
+      </div>
+      <div class="permission-list indented-permission-block">
+        <app-management-permission-item class="subpermission-row"
+            icon="app-management:location" app="[[app_]]"
+            permission-label="Location"
+            permission-type="kLocation">
+        </app-management-permission-item>
+        <app-management-permission-item class="subpermission-row"
+            icon="app-management:camera" app="[[app_]]"
+            permission-label="Camera"
+            permission-type="kCamera">
+        </app-management-permission-item>
+        <app-management-permission-item class="subpermission-row"
+            icon="app-management:microphone" app="[[app_]]"
+            permission-label="Microphone"
+            permission-type="kMicrophone">
+        </app-management-permission-item>
+      </div>
+    </div>
+    <app-management-file-handling-item
+        class="permission-card-row separated-row"
+        app="[[app_]]">
+    </app-management-file-handling-item>
+    <app-management-more-permissions-item
+        class="permission-card-row separated-row" app="[[app_]]"
+        more-permissions-label="More settings and permissions">
+    </app-management-more-permissions-item>
+  </div>
+</div>
+<!--_html_template_end_-->`;
+}
---
+++ app-settings/app.js
@@ -0,0 +1,70 @@
+// Copyright 2022 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+import './strings.m.js';
+import 'chrome://resources/cr_elements/cr_dialog/cr_dialog.js';
+import 'chrome://resources/cr_elements/cr_toolbar/cr_toolbar.js';
+import 'chrome://resources/cr_components/app_management/app_management_shared_style.css.js';
+import 'chrome://resources/cr_components/app_management/file_handling_item.js';
+import 'chrome://resources/cr_components/app_management/more_permissions_item.js';
+import 'chrome://resources/cr_components/app_management/run_on_os_login_item.js';
+import 'chrome://resources/cr_components/app_management/permission_item.js';
+import 'chrome://resources/cr_components/app_management/window_mode_item.js';
+import 'chrome://resources/cr_components/app_management/icons.html.js';
+import 'chrome://resources/cr_components/app_management/uninstall_button.js';
+import 'chrome://resources/cr_components/localized_link/localized_link.js';
+import { BrowserProxy } from 'chrome://resources/cr_components/app_management/browser_proxy.js';
+import { getAppIcon } from 'chrome://resources/cr_components/app_management/util.js';
+import { loadTimeData } from 'chrome://resources/js/load_time_data.m.js';
+import { PolymerElement } from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+import { getTemplate } from './app.html.js';
+// TODO(crbug.com/1294060): Investigate end-to-end WebAppSettings tests
+export class WebAppSettingsAppElement extends PolymerElement {
+    static get is() {
+        return 'web-app-settings-app';
+    }
+    static get template() {
+        return getTemplate();
+    }
+    static get properties() {
+        return {
+            app_: Object,
+            hidden: {
+                type: Boolean,
+                computed: 'appUnready_(app_)',
+                reflectToAttribute: true,
+            },
+            iconUrl_: { type: String, computed: 'getAppIcon_(app_)' },
+            showSearch_: { type: Boolean, value: false, readonly: true },
+        };
+    }
+    connectedCallback() {
+        super.connectedCallback();
+        const urlPath = new URL(document.URL).pathname;
+        if (urlPath.length <= 1) {
+            return;
+        }
+        window.CrPolicyStrings = {
+            controlledSettingPolicy: loadTimeData.getString('controlledSettingPolicy'),
+        };
+        const appId = urlPath.substring(1);
+        BrowserProxy.getInstance().handler.getApp(appId).then((result) => {
+            this.app_ = result.app;
+        });
+        // Listens to app update.
+        const callbackRouter = BrowserProxy.getInstance().callbackRouter;
+        callbackRouter.onAppChanged.addListener(this.onAppChanged_.bind(this));
+    }
+    onAppChanged_(app) {
+        if (this.app_ && app.id === this.app_.id) {
+            this.app_ = app;
+        }
+    }
+    getAppIcon_(app) {
+        return app ? getAppIcon(app) : '';
+    }
+    appUnready_(app) {
+        return !app;
+    }
+}
+customElements.define(WebAppSettingsAppElement.is, WebAppSettingsAppElement);
